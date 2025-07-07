4f3b1897fceed5a78560ff4a42db853f0fa9510c
ZiadSharaf <115091905+ZiadSharaf@users.noreply.github.com>
2025-07-07 19:00:21 +0300

    Delete Day 1 directory

--- Day 1/Mario pyramid.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,5):
-    Spaces=4-i
-    Asterisks=i
-    print(Spaces*" "+Asterisks*"*")
--- Day 1/String Vowel Counter.py
+++
@@ -1,9 +0,0 @@
-String=input("Please enter a string: ")
-Vowels=['a','A','e','E','o','O','i','I','u','U']
-counter=0
-
-for i in String:
-    if i in Vowels:
-        counter+=1
-
-print(f"Number of vowels in '{String}' = {counter}.")
